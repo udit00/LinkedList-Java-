e7ed06f59d751902f3ba4cc82951d782cba20bc5
Udit Nair <uditnair90@gmail.com>
2021-05-15 00:28:34 +0530

    implemented to have N numbers of Linked List

--- src/com/company/linkedList.java
+++ src/com/company/linkedList.java
@@ -1,7 +1,6 @@
 package com.company;
-
-
-import org.w3c.dom.NodeList;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class linkedList {
@@ -14,14 +13,38 @@ public class linkedList {
             }
         }
 
-        public void printList(){
-            Node temp=head;
+        public static void printList(linkedList linkedList){
+            Node temp=linkedList.head;
             while (temp!=null) {
                 System.out.print(temp.data + " -> ");
                 temp = temp.next;
             }
             System.out.println("\n----------------------------------------");
-            return;
+        }
+
+        public static linkedList createLinkedList(){
+            linkedList newList=new linkedList();
+            Scanner sc=new Scanner(System.in);
+            System.out.println("\nPlease enter the number of Nodes you want : \n" +
+                    "[Note: Don't enter a big number as it'll be hard to enter such a huge input]\n" +
+                    "Choice:");
+            int elementCount=sc.nextInt();
+            while (elementCount!=0){
+                elementCount--;
+                System.out.println("Element : ");
+                int element=sc.nextInt();
+                System.out.println("1. Insert "+element+" @ start of linked list.");
+                System.out.println("2. Insert "+element+" @ end of linked list");
+                System.out.println("Choice : ");
+                short choiceEndStart=(short) sc.nextInt();
+                switch (choiceEndStart) {
+                    case 1 -> insertAtStart(newList, element);
+                    case 2 -> insertAtEnd(newList, element);
+                    default -> System.out.println("Since wrong input was given by you, inserting " + element + " @ the start.");
+                }
+            }
+
+            return newList;
         }
         public static void insertAtStart(linkedList linkedList, int data){
             Node newNode=new Node(data);
@@ -46,7 +69,7 @@ public class linkedList {
                 linkedList.head=newNode;
             }
         }
-        private static void createLinkedList(linkedList list, short count){
+        /*private static void createLinkedList(linkedList list, short count){
             Scanner sc=new Scanner(System.in);
             System.out.println("This is Linked List Number "+count+
                     "\nPlease enter the number of Nodes you want : \n" +
@@ -67,73 +90,46 @@ public class linkedList {
                     default -> System.out.println("Since wrong input was given by you, inserting " + element + " @ the start.");
                 }
             }
-        }
-        private static void menu(short countLinkedList, linkedList list1, linkedList list2, linkedList list3){
+        }*/
+        private static void menu(ArrayList<linkedList> arrayLinkedList){
+
             Scanner sc=new Scanner(System.in);
             System.out.println("---------------------------------------------------");
             System.out.println("LINKED LIST IMPLEMENTATION");
-            System.out.println("1. Create a new linked list [ Can Create Only 3 ]");
-            System.out.println("2. Print Linked List [ must remember the number(Linked List) ]");
+            System.out.println("1. Create a new linked list");
+            System.out.println("2. Print Linked List");
             System.out.println("3. Exit");
             short Choice=(short) sc.nextInt();
             switch (Choice) {
                 case 1 -> {
-                    if (countLinkedList > 3) {
-                        System.out.println("Cannot Create linked list more than 3");
-                        menu(countLinkedList, list1, list2, list3);
-                    } else {
-                        switch (countLinkedList) {
-                            case 1:
-                                createLinkedList(list1, countLinkedList);
-                                menu((short) (countLinkedList+1),list1, list2, list3);
-                                break;
-                            case 2:
-                                createLinkedList(list2, countLinkedList);
-                                menu((short) (countLinkedList+1), list1, list2, list3);
-                                break;
-                            case 3:
-                                createLinkedList(list3, countLinkedList);
-                                menu((short) (countLinkedList+1), list1, list2, list3);
-                                break;
-                            default:
-                                System.out.println("Wrong Menu");
-                                menu(countLinkedList, list1, list2, list3);
-                        }
-                    }
+                    arrayLinkedList.add(createLinkedList());
+                    menu(arrayLinkedList);
                 }
                 case 2 -> {
-                    if ((countLinkedList-1) < 1) {
-                        System.out.println("No Linked List has been created yet.");
-                        menu(countLinkedList, list1, list2, list3);
-                    } else {
+                    short choice = (short) sc.nextInt();
+                    if(arrayLinkedList.size()>0 && arrayLinkedList.size()<choice) {
                         System.out.println("Please enter the linked list you want to print.");
                         System.out.println("Choice : ");
-                        short choice = (short) sc.nextInt();
-                        switch (choice) {
-                            case 1 -> list1.printList();
-                            case 2 -> list2.printList();
-                            case 3 -> list3.printList();
-                            default -> {
-                                System.out.println("Wrong Input");
-                                menu(countLinkedList, list1, list2, list3);
-                            }
-                        }
-                        menu( countLinkedList, list1, list2, list3);
+                        printList(arrayLinkedList.get(choice-1));
                     }
+                    else System.out.println("No linked list exist yet, you can make one @ the menu.");
+
+                    menu(arrayLinkedList);
+
                 }
+
                 case 3 -> {
                     System.out.println("Exiting the program");
                 }
                 default -> {
                     System.out.println("Wrong Input.\nReturning Back to Main Menu... [Please select the option from 1 to 3");
-                    menu(countLinkedList, list1, list2, list3);
+                    menu(arrayLinkedList);
                 }
             }
         }
 
         public static void main(String[] args) {
-            linkedList list1, list2, list3;
-            list1=list2=list3=new linkedList();
-            menu((short) 1, list1, list2, list3);
+            ArrayList<linkedList> arrayLinkedList=new ArrayList<>();
+            menu(arrayLinkedList);
         }
     }
